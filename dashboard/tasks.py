33e312ec5a9573a065bf931112e0fafea068f4d1
ccunhafinance <87538894+ccunhafinance@users.noreply.github.com>
2022-03-09 18:59:12 -0300

    Update tasks.py

--- dashboard/tasks.py
+++ dashboard/tasks.py
@@ -2,7 +2,6 @@ from celery import shared_task
 
 
 @shared_task
-def error_handler(request, exc, traceback):
-    print('Task {0} raised exception: {1!r}\n{2!r}'.format(
-          request.id, exc, traceback))
+def rr(request):
+    print('oi')
 
