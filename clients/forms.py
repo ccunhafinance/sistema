0cb1cde5446afe82b3b2f03536349533205f8b66
ccunhafinance <87538894+ccunhafinance@users.noreply.github.com>
2022-05-19 20:28:56 -0300

    s

--- clients/forms.py
+++ clients/forms.py
@@ -8,7 +8,7 @@ class ModalForm(forms.ModelForm):
     email = forms.CharField(label='Email', widget=forms.TextInput(attrs={'type':'email'}))
     telefone = forms.CharField(label='Telefone', widget=forms.TextInput(attrs={'type':'text'}))
     data_nascimento = forms.DateField(label='Data de Nascimento', widget=forms.TextInput(attrs={'type':'date'}))
-    jaContactei = forms.CharField(label='Cliente ja foi contactado?', widget=forms.CheckboxInput(attrs={'type':'checkbox'}))
+    jaContactei = forms.CharField(label='Cliente ja foi contactado?', required=False, widget=forms.CheckboxInput(attrs={'type':'checkbox'}))
     
     class Meta:
         model = Clientes
